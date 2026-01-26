cd6dc032fd5d91d66a2d0b0752370e2fad68b8de
Siraj617 <mohamedsirajily@gmail.com>
2026-01-26 22:44:07 +0530

    Remove WhoWeHelp component from page
    Removed WhoWeHelp component from the page layout.

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Hero />
       <Research />
       <Features />
-      <WhoWeHelp />
+     
       <Services />
       <Methodology />
       <CaseStudies />
